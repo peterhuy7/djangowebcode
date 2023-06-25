c44be2661918e8f97f9b2e88f1f4535bc2b76053
peterhuy7 <116619990+peterhuy7@users.noreply.github.com>
2023-06-25 14:51:46 +0700

    Update settings.py

--- webvegiachoi/webvegiachoi/settings.py
+++ webvegiachoi/webvegiachoi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*f-rc#581btb$525=9_ft)%xf49b49s@sj$6$xvzpgmfp6a6rt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
